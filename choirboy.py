31775539344f52e2c7d30a570952e8723d23fe25
sam97 <sandaruadithya@gmail.com>
2023-12-25 12:17:00 +1100

    made progress,

--- choirboy.py
+++ choirboy.py
@@ -3,28 +3,28 @@ import numpy as np
 import time
 
 # Define the harmonies (intervals) in semitones from the original voice
-harmony_intervals = [1,6,11]  # You can modify this list for different harmonies
+harmony_intervals = [-20, 0, 200]  # You can modify this list for different harmonies
 
 # Parameters for the audio processing
-CHUNK =1024  # Number of frames per buffer
+CHUNK = 1024  # Number of frames per buffer
 FORMAT = pyaudio.paFloat32
 CHANNELS = 1
 RATE = 192000  # Sample rate
 
 # Function to generate the harmonized audio signal
-def generate_harmony(frames, original_signal):
+def generate_harmony(original_signal):
     processed_data = original_signal.copy()
     for interval in harmony_intervals:
-        harmony_signal = np.roll(original_signal, interval * int(RATE / 440))
+        harmony_signal = np.roll(original_signal, int(interval * RATE / 440))
         processed_data += harmony_signal[:len(original_signal)]
-    return frames + processed_data * 0.1  # Applying gain control (adjust as needed)
+    return processed_data * 0.1  # Applying gain control (adjust as needed)
 
 # Function to process the audio stream
 def audio_callback(in_data, frame_count, time_info, status):
     audio_data = np.frombuffer(in_data, dtype=np.float32)
     
     # Process the audio for harmony effect
-    processed_data = generate_harmony(audio_data, audio_data)
+    processed_data = generate_harmony(audio_data)
     
     return processed_data, pyaudio.paContinue
 
